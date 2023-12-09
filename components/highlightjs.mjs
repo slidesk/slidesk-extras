72f14e03effcf5489edcb8183a133535148f3b9d
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-12-09 13:17:14 +0100

    feat: :sparkles: add components/highlightjs

---
+++ components/highlightjs.mjs
@@ -0,0 +1,15 @@
+export default (data) => {
+  let newData = data;
+  [...newData.matchAll(/```(\w*)\n([^`]+)```/gm)].forEach((match) => {
+    newData = newData.replace(
+      match[0],
+      `<pre><code class="language-${match[1]}">${match[2]
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;")
+        .replaceAll("'", "&#039;")}</code></pre>`,
+    );
+  });
+  return newData;
+};
