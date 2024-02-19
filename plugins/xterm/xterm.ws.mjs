8a8e516c9827834b0fbe19193d1c2119955e8d56
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2024-02-19 10:10:16 +0100

    fix: :bug: try catch on xterm

--- plugins/xterm/xterm.ws.mjs
+++ plugins/xterm/xterm.ws.mjs
@@ -1,12 +1,18 @@
 export default async (message) => {
   const { cwd, command, key } = JSON.parse(message);
-  // eslint-disable-next-line no-undef
-  const proc = Bun.spawnSync(command.split(" "), {
-    cwd: cwd ?? process.env.HOME,
-    onExit: () => {},
-  });
-  return {
-    key,
-    result: proc.stdout.toString(),
-  };
+  try {
+    // eslint-disable-next-line no-undef
+    const proc = Bun.spawnSync(command.split(" "), {
+      cwd: cwd ?? process.env.HOME,
+    });
+    return {
+      key,
+      result: proc.stdout.toString(),
+    };
+  } catch (e) {
+    return {
+      key,
+      result: `Error: ${e}`,
+    };
+  }
 };
