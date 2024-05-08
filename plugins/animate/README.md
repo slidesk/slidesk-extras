4c1d6d7564b17e65148abe51003ddd339b6882dc
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2024-05-08 18:25:37 +0200

    feat: :sparkles: review animate plugin

---
+++ plugins/animate/README.md
@@ -0,0 +1,27 @@
+# SliDesk Plugin: Animate
+
+This plugin lets you add a timer to automatically advance to the next slide.
+
+To allow that, you need to add a class on the slide, prefixed with `auto-`.
+
+```
+## A slide with no animation
+
+...
+
+## From .[animate]
+
+This is the element will be animated between both slides
+
+## To .[animate]
+
+This is the element will be animated between both slides
+
+And because this element is present, the element below moves
+
+## An another slide with no animation
+
+...
+
+
+```
--- plugins/animate/animate.js
+++ plugins/animate/animate.js
@@ -6,11 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
       (
         [...e.outerHTML].reduce(
           (s, c) => (Math.imul(31, s) + c.charCodeAt(0)) | 0,
-          0,
+          0
         ) +
         2147483647 +
         1
-      ).toString(16),
+      ).toString(16)
     );
   });
 });
@@ -33,21 +33,25 @@ const animation = ({ duration, timing, draw, done }) => {
 window.slidesk.animateElements = () => {
   if (window.slidesk.currentSlide !== 0) {
     const prevHashes = [
-      ...document.querySelectorAll("section.sd-previous [data-animhash]"),
+      ...document.querySelectorAll(
+        "section.sd-previous.animate [data-animhash]"
+      ),
     ].map((e) => e.getAttribute("data-animhash"));
     const currentHashes = [
-      ...document.querySelectorAll("section.sd-current [data-animhash]"),
+      ...document.querySelectorAll(
+        "section.sd-current.animate [data-animhash]"
+      ),
     ].map((e) => e.getAttribute("data-animhash"));
     currentHashes
       .filter((value) => prevHashes.includes(value))
       .forEach((intersection) => {
         const prevEl = document.querySelector(
-          `section.sd-previous [data-animhash="${intersection}"]`,
+          `section.sd-previous.animate [data-animhash="${intersection}"]`
         );
         const pBCR = prevEl.getBoundingClientRect();
         const pCS = window.getComputedStyle(prevEl);
         const currEl = document.querySelector(
-          `section.sd-current [data-animhash="${intersection}"]`,
+          `section.sd-current.animate [data-animhash="${intersection}"]`
         );
         const cBCR = currEl.getBoundingClientRect();
         currEl.style.opacity = 0;
@@ -56,7 +60,7 @@ window.slidesk.animateElements = () => {
           ghost.style.setProperty(
             key,
             pCS.getPropertyValue(key),
-            pCS.getPropertyPriority(key),
+            pCS.getPropertyPriority(key)
           );
         });
         ghost.style.position = "absolute";
@@ -72,17 +76,29 @@ window.slidesk.animateElements = () => {
           duration: window.slidesk.animationTimer,
           timing: (timeFraction) => timeFraction,
           draw: (progress) => {
-            ghost.style.top = `${pBCR.top + progress * (cBCR.top - pBCR.top)}px`;
-            ghost.style.left = `${pBCR.left + progress * (cBCR.left - pBCR.left)}px`;
-            ghost.style.bottom = `${pBCR.bottom + progress * (cBCR.bottom - pBCR.bottom)}px`;
-            ghost.style.right = `${pBCR.right + progress * (cBCR.right - pBCR.right)}px`;
-            ghost.style.width = `${pBCR.width + progress * (cBCR.width - pBCR.width)}px`;
-            ghost.style.height = `${pBCR.height + progress * (cBCR.height - pBCR.height)}px`;
+            ghost.style.top = `${
+              pBCR.top + progress * (cBCR.top - pBCR.top)
+            }px`;
+            ghost.style.left = `${
+              pBCR.left + progress * (cBCR.left - pBCR.left)
+            }px`;
+            ghost.style.bottom = `${
+              pBCR.bottom + progress * (cBCR.bottom - pBCR.bottom)
+            }px`;
+            ghost.style.right = `${
+              pBCR.right + progress * (cBCR.right - pBCR.right)
+            }px`;
+            ghost.style.width = `${
+              pBCR.width + progress * (cBCR.width - pBCR.width)
+            }px`;
+            ghost.style.height = `${
+              pBCR.height + progress * (cBCR.height - pBCR.height)
+            }px`;
           },
           done: () => {
             currEl.style.opacity = 1;
             window.slidesk.slides[window.slidesk.currentSlide].removeChild(
-              ghost,
+              ghost
             );
           },
         });
