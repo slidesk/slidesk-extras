97a61b9774ccb8d5210893d0bdb92a8b23758ef1
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2024-09-07 11:04:57 +0200

    feat: :sparkles: new version of xterm

--- plugins/xterm/xterm.ws.mjs
+++ plugins/xterm/xterm.ws.mjs
@@ -1,12 +1,14 @@
 export default async (message) => {
   const { cwd, command, key, env } = JSON.parse(message);
-  Bun.$.env(env);
-  Bun.$.cwd(cwd ?? process.env.HOME);
-  const { stdout, stderr, exitCode } = await Bun.$`${command}`
-    .nothrow()
-    .quiet();
+  const pty = Bun.spawn(command, {
+      cwd,
+      env,
+      name: 'xterm-color',
+      cols: 80,
+      rows: 30
+    });
   return {
     key,
-    result: exitCode !== 0 ? stderr.toString() : stdout.toString(),
+    result: await new Response(pty.stdout).text(),
   };
 };
